f22f28b9e45eeac8434641c2e2a35e431ce7463e
ywwhack <yww7289@163.com>
2018-02-27 18:39:02 +0800

    chore: remove greeting in dev mode

--- .electron-vue/dev-runner.js
+++ .electron-vue/dev-runner.js
@@ -145,27 +145,7 @@ function electronLog (data, color) {
   }
 }
 
-function greeting () {
-  const cols = process.stdout.columns
-  let text = ''
-
-  if (cols > 104) text = 'electron-vue'
-  else if (cols > 76) text = 'electron-|vue'
-  else text = false
-
-  if (text) {
-    say(text, {
-      colors: ['yellow'],
-      font: 'simple3d',
-      space: false
-    })
-  } else console.log(chalk.yellow.bold('\n  electron-vue'))
-  console.log(chalk.blue('  getting ready...') + '\n')
-}
-
 function init () {
-  greeting()
-
   Promise.all([startRenderer(), startMain()])
     .then(() => {
       startElectron()
